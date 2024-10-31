d67b65389b1a6f3c6cba42cd4f918b3b217a0661
willmell <145131287+willmell@users.noreply.github.com>
2024-10-31 15:41:02 -0400

    Update DriveWithJoysticks.java

--- src/main/java/frc/robot/commands/DriveWithJoysticks.java
+++ src/main/java/frc/robot/commands/DriveWithJoysticks.java
@@ -1,45 +1,109 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+
+
+
 import java.util.function.Supplier;
+
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import frc.robot.subsystems.drive.Drive;
 import frc.robot.Constants;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
 
-public class DriveWithJoysticks extends Command{
-    Drive drive;
-    Supplier<Double> leftYSupplier;
-    Supplier<Double> leftXSupplier;
-    
+public class DriveWithJoysticks extends Command {
+    private final Drive drive;
+    private final Supplier<Double> leftYSupplier;
+    private final Supplier<Double> leftXSupplier;
+    double Kp;
+    double Ki;
+    double Kd;
+    boolean piding;
+
+    ChassisSpeeds speeds;
+    private final PIDController pidController;
+    Rotation2d targetHeading;
+
+
     public DriveWithJoysticks(
         Drive drive,
         Supplier<Double> leftYSupplier,
         Supplier<Double> leftXSupplier
-    ){
+    
+    ) {
         addRequirements(drive);
+        this.Kp = 1.0;
+        this.Ki = 0.1;
+        this.Kd = 0.0;
         this.drive = drive;
         this.leftYSupplier = leftYSupplier;
         this.leftXSupplier = leftXSupplier;
+        this.piding = false;
+        speeds = new ChassisSpeeds(
+        0.0,
+        0.0,
+        0.0);
+        targetHeading = new Rotation2d();
+       
+
+        
+        this.pidController = new PIDController(Kp, Ki, Kd);
     }
-    
+
     @Override
-    public void execute() {
-        double leftY = leftYSupplier.get();
-        double leftX = leftXSupplier.get();
-
-        ChassisSpeeds speeds = new ChassisSpeeds(
-            applyPreferences(leftY),
-            0, 
-            -applyPreferences(leftX) * 2 // chassisspeeds considers rotating clockwise as positive
-        );
-
-        drive.setVelocity(speeds);
+public void execute() {
+    double leftY = leftYSupplier.get(); 
+    double leftX = leftXSupplier.get(); 
+    Rotation2d currentHeading = drive.getHeading();
+    if( leftX == 0 && leftY != 0 &&  !this.piding){
+        setTargetHeading(currentHeading.getDegrees());
+        this.piding = true;
     }
 
-    public double applyPreferences(double input){
-        if(Math.abs(input) < Constants.ANALOG_DEADZONE){
+    if(leftX != 0){
+        piding = false;
+    }
+
+  
+    double headingError = targetHeading.getDegrees() - currentHeading.getDegrees();
+    double pidOutput = pidController.calculate(headingError);
+
+    if(this.piding){
+    speeds = new ChassisSpeeds(
+        applyPreferences(leftY),
+        0, 
+        -applyPreferences(leftX) * 2 + pidOutput 
+    );
+    }
+
+    else{
+    speeds = new ChassisSpeeds(
+        applyPreferences(leftY),
+        0, 
+        -applyPreferences(leftX) * 2 
+    );
+    }
+
+    drive.setVelocity(speeds);
+    Logger.recordOutput("target heading", this.targetHeading.getDegrees());
+    Logger.recordOutput("Raw X output", -applyPreferences(leftX) * 2);
+    Logger.recordOutput("mathed X output", -applyPreferences(leftX) * 2 + pidOutput );
+  
+}
+
+
+    public double applyPreferences(double input) {
+        if (Math.abs(input) < Constants.ANALOG_DEADZONE) {
             return 0; 
         }
         return Math.signum(input) * Math.pow(Math.abs(input), Constants.JOYSTICK_EXPONENT) * Constants.MAX_SPEED; // 4 meters per second
     }
+
+    public void setTargetHeading(double headingDegrees) {
+        this.targetHeading = Rotation2d.fromDegrees(headingDegrees);
+    }
+    
 }
