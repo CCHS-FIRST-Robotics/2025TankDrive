f020fb4f05e222874d479c066d53007444321d10
willmell <whickey27@concordcarlisle.org>
2024-11-06 17:28:44 -0500

    rand

--- src/main/java/frc/robot/commands/MoveTo.java
+++ src/main/java/frc/robot/commands/MoveTo.java
@@ -3,7 +3,6 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
-import edu.wpi.first.units.Angle;
 import edu.wpi.first.units.Distance;
 import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.Velocity;
