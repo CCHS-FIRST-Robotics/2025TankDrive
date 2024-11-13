1b037777672860ad532556bfae567c52be55d3cf
willmell <whickey27@concordcarlisle.org>
2024-11-13 13:38:46 -0500

    here hope it works

--- src/main/java/frc/robot/subsystems/drive/DriveSideIOSim.java
+++ src/main/java/frc/robot/subsystems/drive/DriveSideIOSim.java
@@ -4,6 +4,7 @@ import static edu.wpi.first.units.Units.*;
 
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import edu.wpi.first.math.controller.*;
+import edu.wpi.first.math.geometry.struct.Rotation2dStruct;
 import edu.wpi.first.math.system.plant.*;
 import edu.wpi.first.units.*;
 import frc.robot.Constants;
@@ -13,15 +14,17 @@ public class DriveSideIOSim implements DriveSideIO {
     private final PIDController PID;
     private final SimpleMotorFeedforward F;
 
-    private final double kP = 10;
+    // recalc gains are in volts per (meters per second)
+    private final double kP = 0;
     private final double kI = 0;
     private final double kD = 0;
     private final double kS = 0;
-    private final double kV = 2.54 * Constants.WHEEL_RADIUS.in(Meters) / Constants.GEAR_RATIO;
-    private final double kA = 0.27 * Constants.WHEEL_RADIUS.in(Meters) / Constants.GEAR_RATIO;
+    private final double kV = 3.6 * Constants.WHEEL_CIRCUMFERENCE.in(Meters) / Constants.GEAR_RATIO;
+    private final double kA = 0.26 * Constants.WHEEL_CIRCUMFERENCE.in(Meters) / Constants.GEAR_RATIO;
 
     Measure<Voltage> appliedVolts = Volts.of(0);
     Measure<Velocity<Angle>> currentSetpoint = RotationsPerSecond.of(0);
+    DriveSideIOInputs inputs = new DriveSideIOInputs();
     
     public DriveSideIOSim(){
         motor = new DCMotorSim(
@@ -41,10 +44,10 @@ public class DriveSideIOSim implements DriveSideIO {
 
     @Override
     public void setVelocity(Measure<Velocity<Angle>> velocity){
-        double volts = PID.calculate(motor.getAngularVelocityRadPerSec(), velocity.in(RadiansPerSecond))
-                       + F.calculate(currentSetpoint.in(RadiansPerSecond));
-        
-        this.setVoltage(Volts.of(volts));
+        this.setVoltage(Volts.of(
+            PID.calculate(inputs.motor1Velocity.in(RotationsPerSecond), velocity.in(RotationsPerSecond))
+            + F.calculate(velocity.in(RotationsPerSecond))
+        ));
         currentSetpoint = velocity;
     }
 
@@ -52,12 +55,18 @@ public class DriveSideIOSim implements DriveSideIO {
     public void updateInputs(DriveSideIOInputs inputs) {
         motor.update(Constants.PERIOD);
 
-        inputs.currentSetpoint = currentSetpoint;
-
         inputs.motor1Current = Amps.of(motor.getCurrentDrawAmps());
-        inputs.motor1Voltage = Volts.of(appliedVolts.in(Volts));
-        inputs.motor1Position = Rotations.of(motor.getAngularPositionRotations());
-        inputs.motor1Velocity = RotationsPerSecond.of(motor.getAngularVelocityRPM() / 60);
+        inputs.motor1Voltage = appliedVolts;
         inputs.motor1Temperature = Celsius.of(0);
+
+        inputs.motor1Position = Rotations.of(motor.getOutput(0)); // gets the position in the units of the gains you give it
+        inputs.motor1Velocity = RotationsPerSecond.of(motor.getOutput(1));
+        inputs.wheelPosition = Rotations.of(inputs.motor1Position.in(Rotations) / Constants.GEAR_RATIO);
+        inputs.wheelVelocity =  RotationsPerSecond.of(inputs.motor1Velocity.in(RotationsPerSecond) / Constants.GEAR_RATIO);
+
+        inputs.currentSetpoint = RotationsPerSecond.of(currentSetpoint.in(RotationsPerSecond));
+        inputs.distanceTraveled = Meters.of(inputs.wheelPosition.in(Rotations) * Constants.WHEEL_CIRCUMFERENCE.in(Meters));
+
+        this.inputs = inputs;
     }
 }
