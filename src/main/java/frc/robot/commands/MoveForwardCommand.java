2a196f7d559cc9f451e084cbfa02b98cbac3629d
Jonathan Li <jonathanli668899@gmail.com>
2024-11-12 21:40:24 -0500

    Math.abs for encoder readings bc in case negative might affect my isFinished checking if readings>totalRotations

--- src/main/java/frc/robot/commands/MoveForwardCommand.java
+++ src/main/java/frc/robot/commands/MoveForwardCommand.java
@@ -26,8 +26,8 @@ public class MoveForwardCommand extends Command {
     @Override
     public void execute() {
         drive.setVelocity(new ChassisSpeeds(0.3, 0, 0));
-        this.leftRotations = -drive.getLeftEncoderRotations().in(Rotations);
-        this.rightRotations = -drive.getRightEncoderRotations().in(Rotations);
+        this.leftRotations = Math.abs(drive.getLeftEncoderRotations().in(Rotations));
+        this.rightRotations = Math.abs(drive.getRightEncoderRotations().in(Rotations));
 
         System.out.println("Left Rotations: " + leftRotations);
         System.out.println("Right Rotations: " + rightRotations);
