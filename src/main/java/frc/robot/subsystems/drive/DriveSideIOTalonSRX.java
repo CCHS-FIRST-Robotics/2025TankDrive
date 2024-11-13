6a009eeb2306d1fd71065f8156dc8c134685f182
willmell <whickey27@concordcarlisle.org>
2024-11-13 13:27:47 -0500

    using alex velo hope it works

--- src/main/java/frc/robot/subsystems/drive/DriveSideIOTalonSRX.java
+++ src/main/java/frc/robot/subsystems/drive/DriveSideIOTalonSRX.java
@@ -8,36 +8,39 @@ import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.InvertType;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.units.*;
 
 public class DriveSideIOTalonSRX implements DriveSideIO {
     private final TalonSRX motor1, motor2;
+    private final PIDController PID;
+    private final SimpleMotorFeedforward F;
     int encoderTicks = 4096;
 
     private final double kP = 0;
     private final double kI = 0;
     private final double kD = 0;
-    private final double kF = 1; // just kV
+    private final double kS = 0;
+    private final double kV = 0.135; // at 88.83 rotations per second, output 12 volts
+    private final double kA = 0;
 
     Measure<Velocity<Angle>> currentSetpoint = RotationsPerSecond.of(0);
+    DriveSideIOInputs inputs = new DriveSideIOInputs();
     
     public DriveSideIOTalonSRX(int id1, int id2, boolean isInverted){
         motor1 = new TalonSRX(id1);
         motor2 = new TalonSRX(id2);
+        PID = new PIDController(kP, kI, kD);
+        F = new SimpleMotorFeedforward(kS, kV, kA);
 
         motor1.configFactoryDefault();
         motor1.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
         motor1.setSelectedSensorPosition(0);
-
-        motor1.config_kP(0, kP, 0);
-		motor1.config_kI(0, kI, 0);
-		motor1.config_kD(0, kD, 0);
-        motor1.config_kF(0, kF, 0);
-
         motor1.setInverted(isInverted);
-
         motor2.follow(motor1);
-        motor2.setInverted(InvertType.FollowMaster); // ! see if this works
+        motor2.setInverted(InvertType.FollowMaster);
     }
     
     @Override
@@ -55,13 +58,13 @@ public class DriveSideIOTalonSRX implements DriveSideIO {
 
     @Override
     public void setVelocity(Measure<Velocity<Angle>> velocity){
-        motor1.set( // this function takes in encoder ticks per 0.1 seconds
-            TalonSRXControlMode.Velocity, 
-            velocity.in(RotationsPerSecond) * encoderTicks * 0.1
-        );
-
+        this.setVoltage(Volts.of(
+            PID.calculate(inputs.motor1Velocity.in(RotationsPerSecond), velocity.in(RotationsPerSecond))
+            + F.calculate(velocity.in(RotationsPerSecond))
+        ));
         currentSetpoint = velocity;
     }
+
 
     @Override
     public void updateInputs(DriveSideIOInputs inputs) {
