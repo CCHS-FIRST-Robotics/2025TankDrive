a7ac796dc32b2fe72b31ee57d078b3928ed7601e
willmell <whickey27@concordcarlisle.org>
2024-11-13 19:27:28 -0500

    one less method in drive

--- src/main/java/frc/robot/commands/MoveTo.java
+++ src/main/java/frc/robot/commands/MoveTo.java
@@ -3,9 +3,16 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.Rotations;
+
+
+import edu.wpi.first.units.Angle;
 import edu.wpi.first.units.Distance;
 import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.Velocity;
+import frc.robot.Constants;
 import frc.robot.subsystems.drive.Drive;
 
 public class MoveTo extends Command {
@@ -14,35 +21,45 @@ public class MoveTo extends Command {
     Measure<Velocity<Distance>> Mps;
     Measure<Distance> y;
     boolean finished;
+    Measure<Angle> target_Rotations;
+    Measure<Angle> target_Angle;
     
 
     public MoveTo(
         Drive drive, 
         Measure<Distance> x, 
         Measure<Distance> y, 
-        Measure<Velocity<Distance>> Mps)
-     {
+        Measure<Velocity<Distance>> Mps) {
 
         this.drive = drive;
         this.x = x;
         this.Mps = Mps;
         this.y = y;
         this.finished = false;
+        this.target_Rotations = Rotations.of(0);
+        this.target_Angle = Degrees.of(0);
 
     }
 
     @Override
     public void initialize() {
+        drive.setDriveBrakeMode(true);
+
+        target_Rotations = Rotations.of(-((drive.getLeftRotations().in(Rotations) + drive.getRightRotations().in(Rotations)) / 2) + ((Math.sqrt(Math.pow(x.in(Meters), 2) + Math.pow(y.in(Meters), 2))) / Constants.WHEEL_CIRCUMFERENCE.in(Meters)));
+        target_Angle = Degrees.of(Math.atan2(y.in(Meters), x.in(Meters)));
     }
 
     @Override
     public void execute() {
+        
+        finished = drive.goForward(target_Angle, Mps, target_Rotations);
 
     }
 
     @Override
     public boolean isFinished() {
-        return drive.goToPositsion(x,y,Mps);
+
+        return finished;
      
     }
 }
