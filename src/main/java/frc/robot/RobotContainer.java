c3b9b5c1ffddd4bccd2a7b6d797822bc628106da
Jonathan Li <jonathanli668899@gmail.com>
2024-12-23 22:00:40 -0500

    help pls

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -54,11 +54,11 @@ public class RobotContainer {
     }
 
     public Command getAutonomousCommand() {
-         return new MoveForwardCommand(drive, 2)
+         return new MoveForwardCommand(drive, 2, 3, 2)
          .andThen(new Turn(drive, -90))
-         .andThen(new MoveForwardCommand(drive, 1.5))
+         .andThen(new MoveForwardCommand(drive, 1.5, 3, 2))
          .andThen(new Turn(drive, -180))
-         .andThen(new MoveForwardCommand(drive, 2));
+         .andThen(new MoveForwardCommand(drive, 2, 3, 2));
     }
     
 
